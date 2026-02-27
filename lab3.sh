78310b3c1ad33cd0c45a6cf445aedade93a64377
H Pranav Amruth <hpranavamruthbca25@rvu.edu.in>
2026-02-27 12:51:51 +0530

    Add files via upload

---
+++ lab3.sh
@@ -0,0 +1,22 @@
+echo "job1:Display Date"
+date 
+sleep 3
+
+echo "job2:Display Current User"
+whoami
+sleep 3
+
+echo "job3:Display Calendar"
+cat
+sleep 2
+
+echo "job4:List In Files"
+ls 
+sleep 3
+
+echo "job5:Display My System Date"
+date +"%T"
+sleep 1
+
+echo "job6"
+pwd
